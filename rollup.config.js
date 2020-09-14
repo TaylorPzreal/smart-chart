e93cd6db96da6312016fcc377cbcca62b65665e8
TaylorPzreal <taylorpzreal@gmail.com>
2020-09-14 19:24:24 +0800

    chore: add map

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2';
 import {terser} from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
+import json from '@rollup/plugin-json';
 
 const baseOpts = {
   input: 'src/index.ts',
@@ -11,6 +12,7 @@ const baseOpts = {
 
 const configurePlugins = ({module}) => {
   return [
+    json(),
     typescript(),
     babel({
       presets: [['@babel/preset-env', {
@@ -33,17 +35,19 @@ export default [
     ...baseOpts,
     output: {
       format: 'esm',
-      file: './dist/index.es.js',
+      // file: './dist/index.es.js',
+      dir: './dist',
     },
     plugins: configurePlugins({module: true}),
   },
-  {
-    ...baseOpts,
-    output: {
-      format: 'umd',
-      file: './dist/index.umd.js',
-      name: 'SmartChart',
-    },
-    plugins: configurePlugins({module: false}),
-  },
+  // {
+  //   ...baseOpts,
+  //   output: {
+  //     format: 'umd',
+  //     // file: './dist/index.umd.js',
+  //     dir: './dist',
+  //     name: 'SmartChart',
+  //   },
+  //   plugins: configurePlugins({module: false}),
+  // },
 ];
--- src/charts/index.ts
+++ src/charts/index.ts
@@ -1,3 +1,5 @@
 export * from './bar';
 export * from './line';
 export * from './pie';
+export * from './map';
+export * from './china-map';
